3cce3d5a1035fed67744935d68bfd32490a2eb0f
Velpuri Shiva Rama Krishna Chaithanya <2023aa05912@wilp.bits-pilani.ac.in>
2026-03-09 09:39:26 +0530

    added about functionality

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ async def root():
 def home():
     return { 'message' : 'Welcome to out home page. redirect to /docs for interactive workflows'}
 
+@app.get('/about')
+def about():
+    return { 'message' : 'This is a simple FastAPI application to demonstrate how to create a web server using FastAPI. It has two endpoints: /home and /about. The /home endpoint returns a welcome message, while the /about endpoint provides information about the application.' }
+
 if __name__ == "__main__":
     import uvicorn
 
