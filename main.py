88c4625d9414686122bf1416680cf7ee9e999ce4
binarybridge-trainings <binarybridge.trainings@gmail.com>
2026-03-09 09:39:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ async def root():
 def home():
     return { 'message' : 'Welcome to out home page. redirect to /docs for interactive workflows'}
 
+
+@app.post('/create')
+def create_user():
+    return { 'message' : 'user created successfully'}
+
+
+
 if __name__ == "__main__":
     import uvicorn
 
